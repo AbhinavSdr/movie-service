4db4e42f8bc57e32e1415215393af294a69a38fb
Abhinav Sudhakar <28253243+AbhinavSdr@users.noreply.github.com>
2025-01-09 09:55:15 +0530

    first commit

---
+++ src/main/java/com/demo/movie_service/pojo/MoviePojo.java
@@ -0,0 +1,33 @@
+package com.demo.movie_service.pojo;
+
+import java.time.LocalDate;
+
+import jakarta.annotation.Nonnull;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class MoviePojo {
+
+    private int movie_id;
+
+    @NotBlank
+    private String movieTitle;
+
+    @NotBlank
+    private String movieDesc;
+
+    @NotBlank
+    private String movieLanguage;
+
+    @NotNull
+    private LocalDate movieReleaseDate;
+
+    private String movieImageUrl;
+}
